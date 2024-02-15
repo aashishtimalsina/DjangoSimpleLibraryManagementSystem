f5603484fd48c8405673460ad19e951cb5cdc3e1
aashishtimalsina <tm.aashish1@gmail.com>
2024-02-15 21:19:42 +0545

    email done

--- core/settings.py
+++ core/settings.py
@@ -149,3 +149,10 @@ MEDIA_URL = '/covers/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'sesemi0129@gmail.com'
+EMAIL_HOST_PASSWORD = 'fuoluxzcopitidov'
+EMAIL_USE_TLS = True
--- web/views.py
+++ web/views.py
@@ -5,7 +5,8 @@ from django.contrib import messages
 from bookstore.models import book
 from introduction.models import introduction
 from django.http import JsonResponse
-
+from django.core.mail import send_mail
+from django.conf import settings
 
 def home(request):
     books = book.objects.all()  
@@ -26,11 +27,33 @@ def contact(request):
         form = ContactMessageForm()
     return render(request, 'contact.html', {'form': form})
 
+# def order_book(request):
+#     if request.method == 'POST':
+#         form = BookOrderForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             return JsonResponse({'message': 'Order placed successfully'})
+#         else:
+#             return JsonResponse({'errors': form.errors}, status=400)
+
 def order_book(request):
     if request.method == 'POST':
         form = BookOrderForm(request.POST)
         if form.is_valid():
-            form.save()
+            instance = form.save()
+            send_instant_reply_email(instance) 
             return JsonResponse({'message': 'Order placed successfully'})
         else:
             return JsonResponse({'errors': form.errors}, status=400)
+
+def send_instant_reply_email(order_instance):
+    subject = 'Hamro Books'
+    message = 'Thank you for your book order! We will be in touch with you shortly'
+    from_email = settings.EMAIL_HOST_USER
+    to_email = [order_instance.email]
+    
+    try:
+        send_mail(subject, message, from_email, to_email)  
+        print("Instant reply email sent successfully.")
+    except Exception as e:
+        print("An error occurred while sending the instant reply email:", e)
